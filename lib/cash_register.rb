5d66b6d6bf2d65e9fb42018ddfade34c3f79b355
Charles Butler <cdbutle@outlook.com>
2020-03-27 13:44:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,7 +6,7 @@ class CashRegister
   attr_accessor :total, :discount, :price, :items,
 
   def initialize(discount = 0)
-    @total = 0.0 
+    @total = 0
     @discount = discount
     @items = []
   end
